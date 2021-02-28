40506470b455ec4bd88e8adc99c925cdff654318
rmgoo <rm.gooogle@gmail.com>
2021-02-28 13:46:28 +0300

    Add CRUD service, refactor other serv

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
@@ -1,16 +1,11 @@
 package guru.springframework.sfgpetclinic.service;
 
-import guru.springframework.sfgpetclinic.model.Owner;
+
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
 
-    Pet findById(long id);
+public interface PetService extends CrudService<Pet, Long>{
 
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/VetSevice.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/VetSevice.java
@@ -3,12 +3,8 @@ package guru.springframework.sfgpetclinic.service;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetSevice {
-    Vet findById(long id);
 
-    Vet save(Vet vet);
+public interface VetSevice extends CrudService<Vet, Long> {
 
-    Set<Vet> findAll();
 }
