ade3d676689bd6ce53871a8c60f889f719a8988a
rmgoo <rm.gooogle@gmail.com>
2021-03-12 17:09:51 +0300

    added properties to Owner

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -6,7 +6,34 @@ import java.util.Set;
  * Created by jt on 7/13/18.
  */
 public class Owner extends Person {
+    private String address;
+    private String telephone;
+    private String city;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 
     public Set<Pet> getPets() {
         return pets;
