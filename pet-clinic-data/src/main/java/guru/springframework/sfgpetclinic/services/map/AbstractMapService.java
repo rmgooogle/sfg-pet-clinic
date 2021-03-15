4688dd779362b6f0c4c1294b5b435a9de845d494
rmgoo <rm.gooogle@gmail.com>
2021-03-15 10:32:05 +0300

    added pets

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -4,8 +4,9 @@ import guru.springframework.sfgpetclinic.model.BaseEntity;
 
 import java.util.*;
 
-public class AbstractMapService <T extends BaseEntity,ID extends Long>{
-    protected Map<Long,T> map = new HashMap<>();
+public abstract class AbstractMapService <T extends BaseEntity,ID extends Long>{
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
